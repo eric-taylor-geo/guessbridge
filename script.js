4d4a5b67ce9759b49f823abfe17b8688470fb660
Eric Taylor <erictaylorgeo@gmail.com>
2025-09-22 22:19:25 +0100

    Added splash

--- script.js
+++ script.js
@@ -1,280 +1,61 @@
 const images = [
-    {
-      "filename": "10.jpeg",
-      "lat": 52.193997,
-      "lon": 0.124803
-    },
-    {
-      "filename": "26.jpeg",
-      "lat": 52.205868, 
-      "lon": 0.114500
-    },
-    {
-      "filename": "30.jpeg",
-      "lat": 52.199233,
-      "lon": 0.115814
-    },
-    {
-      "filename": "31.jpeg",
-      "lat": 52.202413, 
-      "lon": 0.118798
-    },
-    {
-      "filename": "27.jpeg",
-      "lat": 52.205944,
-      "lon": 0.127031
-    },
-    {
-      "filename": "11.jpeg",
-      "lat": 52.204047, 
-      "lon": 0.125120
-    },
-    {
-      "filename": "20.jpeg",
-      "lat": 52.202336,
-      "lon": 0.120297
-    },
-    {
-      "filename": "36.jpeg",
-      "lat": 52.200311, 
-      "lon": 0.114009
-    },
-    {
-      "filename": "41.jpeg",
-      "lat": 52.257644,
-      "lon": 0.198883
-    },
-    {
-      "filename": "16.jpeg",
-      "lat": 52.211692,
-      "lon": 0.118378
-    },
-    {
-      "filename": "6.jpeg",
-      "lat": 52.211964,
-      "lon": 0.119814
-    },
-    {
-      "filename": "7.jpeg",
-      "lat": 52.20317784107344, 
-      "lon": 0.11755440753907162
-    },
-    {
-      "filename": "17.jpeg",
-      "lat": 52.20965572240101, 
-      "lon": 0.11680101341970639
-    },
-    {
-      "filename": "40.jpeg",
-      "lat": 52.199647,
-      "lon": 0.106214
-    },
-    {
-      "filename": "37.jpeg",
-      "lat": 52.203622,
-      "lon": 0.114275
-    },
-    {
-      "filename": "21.jpeg",
-      "lat": 52.199611,
-      "lon": 0.115794
-    },
-    {
-      "filename": "34.jpeg",
-      "lat": 52.20878588570246, 
-      "lon": 0.11504434206182547
-    },
-    {
-      "filename": "8.jpeg",
-      "lat": 52.206036,
-      "lon": 0.11785
-    },
-    {
-      "filename": "22.jpeg",
-      "lat": 52.205794,
-      "lon": 0.113369
-    },
-    {
-      "filename": "18.jpeg",
-      "lat": 52.210536,
-      "lon": 0.121533
-    },
-    {
-      "filename": "38.jpeg",
-      "lat": 52.200683,
-      "lon": 0.113933
-    },
-    {
-      "filename": "4.jpeg",
-      "lat": 52.205437, 
-      "lon": 0.115872
-    },
-    {
-      "filename": "14.jpeg",
-      "lat": 52.209264,
-      "lon": 0.118486
-    },
-    {
-      "filename": "15.jpeg",
-      "lat": 52.212397,
-      "lon": 0.119972
-    },
-    {
-      "filename": "5.jpeg",
-      "lat": 52.202961,
-      "lon": 0.117644
-    },
-    {
-      "filename": "39.jpeg",
-      "lat": 52.200386,
-      "lon": 0.107517
-    },
-    {
-      "filename": "19.jpeg",
-      "lat": 52.200797,
-      "lon": 0.114386
-    },
-    {
-      "filename": "23.jpeg",
-      "lat": 52.210889, 
-      "lon": 0.117809
-    },
-    {
-      "filename": "9.jpeg",
-      "lat": 52.210525,
-      "lon": 0.117867
-    },
-    {
-      "filename": "35.jpeg",
-      "lat": 52.207597, 
-      "lon": 0.114948
-    },
-    {
-      "filename": "2.jpeg",
-      "lat": 52.203867, 
-      "lon": 0.124781
-    },
-    {
-      "filename": "28.jpeg",
-      "lat": 52.21255,
-      "lon": 0.120931
-    },
-    {
-      "filename": "12.jpeg",
-      "lat": 52.202166, 
-      "lon": 0.115545
-    },
-    {
-      "filename": "32.jpeg",
-      "lat": 52.201931, 
-      "lon": 0.118188
-    },
-    {
-      "filename": "24.jpeg",
-      "lat": 52.180397, 
-      "lon": 0.096781
-    },
-    {
-      "filename": "25.jpeg",
-      "lat": 52.209703,
-      "lon": 0.118911
-    },
-    {
-      "filename": "33.jpeg",
-      "lat": 52.205979413011576, 
-      "lon": 0.12360179414209103
-    },
-    {
-      "filename": "13.jpeg",
-      "lat": 52.200844,
-      "lon": 0.119239
-    },
-    {
-      "filename": "29.jpeg",
-      "lat": 52.198392,
-      "lon": 0.126
-    },
-    {
-      "filename": "3.jpeg",
-      "lat": 52.211136,
-      "lon": 0.117889
-    },
-    {
-        "filename": "42.jpeg",
-        "lat": 52.203006,
-        "lon": 0.1190362
-    },
-    {
-        "filename": "43.jpeg",
-        "lat": 52.203669, 
-        "lon": 0.118613
-    },
-    {
-        "filename": "44.jpeg",
-        "lat": 52.202448, 
-        "lon": 0.119744
-    },
-    {
-        "filename": "45.jpeg",
-        "lat": 52.208024, 
-        "lon": 0.093563
-    },
-    {
-        "filename": "46.jpeg",
-        "lat": 52.199486, 
-        "lon": 0.104559
-    },
-    {
-        "filename": "47.jpeg",
-        "lat": 52.190322, 
-        "lon": 0.136628
-    },
-    {
-        "filename": "48.jpeg",
-        "lat": 52.199602, 
-        "lon": 0.119318
-    },
-    {
-        "filename": "49.jpeg",
-        "lat": 52.203513,
-        "lon": 0.112778
-    },
-    {
-        "filename": "50.jpeg",
-        "lat": 52.199519, 
-        "lon": 0.105076
-    },
-    {
-        "filename": "51.jpeg",
-        "lat": 52.207058, 
-        "lon": 0.097494
-    },
-    {
-        "filename": "52.jpeg",
-        "lat": 52.203883, 
-        "lon": 0.115890
-    },
-    {
-        "filename": "53.jpeg",
-        "lat": 52.20629827538156, 
-        "lon": 0.11798831370915676
-    },
-    {
-        "filename": "54.jpeg",
-        "lat": 52.20692757317422, 
-        "lon": 0.12400214780824373
-    },
-    {
-        "filename": "55.jpeg",
-        "lat": 52.20840407773925,
-        "lon": 0.11747381716110947
-    },
-    {
-        "filename": "56.jpeg",
-        "lat": 52.211417562951674, 
-        "lon": 0.11675767072568762
-    }
-]
+  { "filename": "10.jpeg", "lat": 52.193997, "lon": 0.124803 },
+  { "filename": "26.jpeg", "lat": 52.205868, "lon": 0.114500 },
+  { "filename": "30.jpeg", "lat": 52.199233, "lon": 0.115814 },
+  { "filename": "31.jpeg", "lat": 52.202413, "lon": 0.118798 },
+  { "filename": "27.jpeg", "lat": 52.205944, "lon": 0.127031 },
+  { "filename": "11.jpeg", "lat": 52.204047, "lon": 0.125120 },
+  { "filename": "20.jpeg", "lat": 52.202336, "lon": 0.120297 },
+  { "filename": "36.jpeg", "lat": 52.200311, "lon": 0.114009 },
+  { "filename": "41.jpeg", "lat": 52.257644, "lon": 0.198883 },
+  { "filename": "16.jpeg", "lat": 52.211692, "lon": 0.118378 },
+  { "filename": "6.jpeg", "lat": 52.211964, "lon": 0.119814 },
+  { "filename": "7.jpeg", "lat": 52.20317784107344, "lon": 0.11755440753907162 },
+  { "filename": "17.jpeg", "lat": 52.20965572240101, "lon": 0.11680101341970639 },
+  { "filename": "40.jpeg", "lat": 52.199647, "lon": 0.106214 },
+  { "filename": "37.jpeg", "lat": 52.203622, "lon": 0.114275 },
+  { "filename": "21.jpeg", "lat": 52.199611, "lon": 0.115794 },
+  { "filename": "34.jpeg", "lat": 52.20878588570246, "lon": 0.11504434206182547 },
+  { "filename": "8.jpeg", "lat": 52.206036, "lon": 0.11785 },
+  { "filename": "22.jpeg", "lat": 52.205794, "lon": 0.113369 },
+  { "filename": "18.jpeg", "lat": 52.210536, "lon": 0.121533 },
+  { "filename": "38.jpeg", "lat": 52.200683, "lon": 0.113933 },
+  { "filename": "4.jpeg", "lat": 52.205437, "lon": 0.115872 },
+  { "filename": "14.jpeg", "lat": 52.209264, "lon": 0.118486 },
+  { "filename": "15.jpeg", "lat": 52.212397, "lon": 0.119972 },
+  { "filename": "5.jpeg", "lat": 52.202961, "lon": 0.117644 },
+  { "filename": "39.jpeg", "lat": 52.200386, "lon": 0.107517 },
+  { "filename": "19.jpeg", "lat": 52.200797, "lon": 0.114386 },
+  { "filename": "23.jpeg", "lat": 52.210889, "lon": 0.117809 },
+  { "filename": "9.jpeg", "lat": 52.210525, "lon": 0.117867 },
+  { "filename": "35.jpeg", "lat": 52.207597, "lon": 0.114948 },
+  { "filename": "2.jpeg", "lat": 52.203867, "lon": 0.124781 },
+  { "filename": "28.jpeg", "lat": 52.21255, "lon": 0.120931 },
+  { "filename": "12.jpeg", "lat": 52.202166, "lon": 0.115545 },
+  { "filename": "32.jpeg", "lat": 52.201931, "lon": 0.118188 },
+  { "filename": "24.jpeg", "lat": 52.180397, "lon": 0.096781 },
+  { "filename": "25.jpeg", "lat": 52.209703, "lon": 0.118911 },
+  { "filename": "33.jpeg", "lat": 52.205979413011576, "lon": 0.12360179414209103 },
+  { "filename": "13.jpeg", "lat": 52.200844, "lon": 0.119239 },
+  { "filename": "29.jpeg", "lat": 52.198392, "lon": 0.126 },
+  { "filename": "3.jpeg", "lat": 52.211136, "lon": 0.117889 },
+  { "filename": "42.jpeg", "lat": 52.203006, "lon": 0.1190362 },
+  { "filename": "43.jpeg", "lat": 52.203669, "lon": 0.118613 },
+  { "filename": "44.jpeg", "lat": 52.202448, "lon": 0.119744 },
+  { "filename": "45.jpeg", "lat": 52.208024, "lon": 0.093563 },
+  { "filename": "46.jpeg", "lat": 52.199486, "lon": 0.104559 },
+  { "filename": "47.jpeg", "lat": 52.190322, "lon": 0.136628 },
+  { "filename": "48.jpeg", "lat": 52.199602, "lon": 0.119318 },
+  { "filename": "49.jpeg", "lat": 52.203513, "lon": 0.112778 },
+  { "filename": "50.jpeg", "lat": 52.199519, "lon": 0.105076 },
+  { "filename": "51.jpeg", "lat": 52.207058, "lon": 0.097494 },
+  { "filename": "52.jpeg", "lat": 52.203883, "lon": 0.115890 },
+  { "filename": "53.jpeg", "lat": 52.20629827538156, "lon": 0.11798831370915676 },
+  { "filename": "54.jpeg", "lat": 52.20692757317422, "lon": 0.12400214780824373 },
+  { "filename": "55.jpeg", "lat": 52.20840407773925, "lon": 0.11747381716110947 },
+  { "filename": "56.jpeg", "lat": 52.211417562951674, "lon": 0.11675767072568762 }
+];
+
 let current = null;
 let guessMarker = null;
 let actualMarker = null;
@@ -284,136 +65,111 @@ let timeLeft = 30;
 let accuracyCircle = null;
 let guessLine = null;
 
-
 // Scoring
 let totalGuesses = 0;
 let correctGuesses = 0;
 let totalDistance = 0;
 
 const map = L.map('map', {
-    center: [52.205, 0.119],
-    zoom: 15,
-    zoomSnap: 0.5  // allow 0.5 steps
-  });
+  center: [52.205, 0.119],
+  zoom: 15,
+  zoomSnap: 0.5
+});
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
 const redIcon = new L.Icon({
-    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.3/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
-  });
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.3/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
 
 let isGuessingAllowed = true;
 let nextImageData = null;
-
 let shuffledImages = [];
 let currentImageIndex = 0;
 
 map.on('click', function (e) {
-if (!isGuessingAllowed) return; 
+  if (!isGuessingAllowed) return;
   guessLatLng = e.latlng;
   if (guessMarker) {
     guessMarker.setLatLng(guessLatLng);
   } else {
-    guessMarker = L.marker(guessLatLng).addTo(map).bindPopup("Your Guess")//.openPopup();
+    guessMarker = L.marker(guessLatLng).addTo(map).bindPopup("Your Guess");
   }
   document.getElementById("feedback").innerText = "Click 'Submit Guess' to confirm.";
-    // Enable the submit button now
-    const submitBtn = document.getElementById("submitBtn");
-    submitBtn.disabled = false;
-    submitBtn.classList.remove("disabled");
+  const submitBtn = document.getElementById("submitBtn");
+  submitBtn.disabled = false;
+  submitBtn.classList.remove("disabled");
 });
 
 document.getElementById("submitBtn").addEventListener("click", function () {
-    if (!guessLatLng || !current) {
-    //   alert("Please make a guess by clicking on the map first.");
-      return;
-    }
-  
-    this.disabled = true; // Disable the button
-    this.classList.add("disabled"); 
-  
-    clearInterval(timer);
-    handleGuess(guessLatLng);
-  });
-  function preloadNextImage(index) {
-    // Wrap around if we go out of bounds
-    if (index >= shuffledImages.length) {
-      index = 0;
-    }
-  
-    nextImageData = shuffledImages[index];
-  
-    const img = new Image();
-    img.src = `images/${nextImageData.filename}`;
+  if (!guessLatLng || !current) return;
+  this.disabled = true;
+  this.classList.add("disabled");
+  clearInterval(timer);
+  handleGuess(guessLatLng);
+});
+
+function preloadNextImage(index) {
+  if (index >= shuffledImages.length) {
+    index = 0;
   }
-  
-  
-  
-  
-  
+  nextImageData = shuffledImages[index];
+  const img = new Image();
+  img.src = `images/${nextImageData.filename}`;
+}
+
 function handleGuess(latlng, timedOut = false) {
   const actual = L.latLng(current.lat, current.lon);
-  const dist = map.distance(latlng, actual); // meters
+  const dist = map.distance(latlng, actual);
 
   if (actualMarker) map.removeLayer(actualMarker);
-  actualMarker = L.marker(actual, { icon: redIcon }).addTo(map).bindPopup("Actual Location")//.openPopup();
+  actualMarker = L.marker(actual, { icon: redIcon }).addTo(map).bindPopup("Actual Location");
 
-  if (accuracyCircle) {
-    map.removeLayer(accuracyCircle);
-  }
+  if (accuracyCircle) map.removeLayer(accuracyCircle);
+
   if (!timedOut && guessLatLng) {
     const midLat = (guessLatLng.lat + actual.lat) / 2;
     const midLng = (guessLatLng.lng + actual.lng) / 2;
-    const dist = map.distance(guessLatLng, actual); // in meters
+    const dist = map.distance(guessLatLng, actual);
     const zoom = getZoomLevel(dist);
-  
     map.setView([midLat, midLng], zoom);
   }
   if (timedOut && !guessLatLng) {
     map.setView([current.lat, current.lon], 16);
   }
 
-  // Remove old line if it exists
-    if (guessLine) {
-        map.removeLayer(guessLine);
-    }
-    
-    // Draw a line between the guess and the actual location
-    if (!timedOut && guessLatLng) {
-        guessLine = L.polyline([guessLatLng, actual], {
-        color: 'black',
-        weight: 2,
-        opacity: 0.7,
-        dashArray: '5,5'
-        }).addTo(map);
-    }
-  
-  
-  
-  // Draw the 15m accuracy circle
-    accuracyCircle = L.circle(actual, {
+  if (guessLine) map.removeLayer(guessLine);
+  if (!timedOut && guessLatLng) {
+    guessLine = L.polyline([guessLatLng, actual], {
+      color: 'black',
+      weight: 2,
+      opacity: 0.7,
+      dashArray: '5,5'
+    }).addTo(map);
+  }
+
+  accuracyCircle = L.circle(actual, {
     radius: 50,
     color: '#007bff',
     fillColor: '#007bff',
     fillOpacity: 0.2
-    }).addTo(map);
+  }).addTo(map);
 
   if (!timedOut) {
     totalGuesses++;
     totalDistance += dist;
     if (dist <= 50) correctGuesses++;
   }
-  // Disable the submit button after guess or timeout
-    const submitBtn = document.getElementById("submitBtn");
-    submitBtn.disabled = true;
-    submitBtn.classList.add("disabled");
+
+  const submitBtn = document.getElementById("submitBtn");
+  submitBtn.disabled = true;
+  submitBtn.classList.add("disabled");
 
   const avgDist = totalGuesses > 0 ? (totalDistance / totalGuesses).toFixed(1) : "N/A";
-
   document.getElementById('feedback').innerHTML =
     timedOut
       ? `Time's up!<br>${correctGuesses} / ${totalGuesses} correct guesses (≤ 50m)<br>Average distance: ${avgDist} meters`
@@ -421,7 +177,6 @@ function handleGuess(latlng, timedOut = false) {
 
   document.getElementById("timer").innerText = "";
   isGuessingAllowed = false;
-
 }
 
 function startTimer() {
@@ -431,89 +186,82 @@ function startTimer() {
   timer = setInterval(() => {
     timeLeft--;
     document.getElementById("timer").innerText = `Time left: ${timeLeft}s`;
-
     if (timeLeft <= 0) {
       clearInterval(timer);
-      handleGuess(L.latLng(0, 0), true); // Use dummy guess so it's counted as incorrect only
+      handleGuess(L.latLng(0, 0), true);
     }
   }, 1000);
 }
 
 function nextImage() {
-    if (accuracyCircle) {
-        map.removeLayer(accuracyCircle);
-      }
-    if (guessLine) {
-        map.removeLayer(guessLine);
-    }
-    clearInterval(timer);
-    document.getElementById("feedback").innerText = '';
-    document.getElementById("timer").innerText = '';
-  
-    map.eachLayer(layer => {
-      if (layer instanceof L.Marker || layer instanceof L.Popup) {
-        map.removeLayer(layer);
-      }
-    });
-  
-    map.setView([52.205, 0.119], 15);
-  
-    // Reshuffle if at end of image list
-    if (currentImageIndex >= shuffledImages.length) {
-      shuffleImages();
-    }
-  
-    // Show current image
-    current = shuffledImages[currentImageIndex];
-    document.getElementById('photo').src = `images/${current.filename}`;
-  
-    // Preload the next image for faster loading times
-    preloadNextImage(currentImageIndex + 1);
-  
-    // Advance the index AFTER using current
-    currentImageIndex++;
-  
-    // Reset game state
-    guessLatLng = null;
-    guessMarker = null;
-    actualMarker = null;
-    isGuessingAllowed = true;
-  
-    const submitBtn = document.getElementById("submitBtn");
-    submitBtn.disabled = true;
-    submitBtn.classList.add("disabled");
-  
-    startTimer();
-    map.invalidateSize();
+  if (accuracyCircle) map.removeLayer(accuracyCircle);
+  if (guessLine) map.removeLayer(guessLine);
+  clearInterval(timer);
+  document.getElementById("feedback").innerText = '';
+  document.getElementById("timer").innerText = '';
 
-  }
-  
-  
-  
-  function shuffleImages() {
-    shuffledImages = [...images];
-  
-    for (let i = shuffledImages.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [shuffledImages[i], shuffledImages[j]] = [shuffledImages[j], shuffledImages[i]];
+  map.eachLayer(layer => {
+    if (layer instanceof L.Marker || layer instanceof L.Popup) {
+      map.removeLayer(layer);
     }
-    
-    currentImageIndex = 0;
+  });
+
+  map.setView([52.205, 0.119], 15);
+
+  if (currentImageIndex >= shuffledImages.length) {
+    shuffleImages();
   }
 
-  function getZoomLevel(distance) {
-    if (distance < 20) return 19;
-    if (distance < 50) return 18;
-    if (distance < 100) return 17;
-    if (distance < 250) return 16;
-    if (distance < 500) return 15;
-    if (distance < 750) return 14.5;
-    if (distance < 1000) return 14;
-    if (distance < 1500) return 13.5;
-    if (distance < 2000) return 13;
-    return 12; // fallback for larger distances
-  }
+  current = shuffledImages[currentImageIndex];
+  document.getElementById('photo').src = `images/${current.filename}`;
+  preloadNextImage(currentImageIndex + 1);
+  currentImageIndex++;
 
+  guessLatLng = null;
+  guessMarker = null;
+  actualMarker = null;
+  isGuessingAllowed = true;
+
+  const submitBtn = document.getElementById("submitBtn");
+  submitBtn.disabled = true;
+  submitBtn.classList.add("disabled");
+
+  startTimer();
+  map.invalidateSize();
+}
+
+function shuffleImages() {
+  shuffledImages = [...images];
+  for (let i = shuffledImages.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledImages[i], shuffledImages[j]] = [shuffledImages[j], shuffledImages[i]];
+  }
+  currentImageIndex = 0;
+}
+
+function getZoomLevel(distance) {
+  if (distance < 20) return 19;
+  if (distance < 50) return 18;
+  if (distance < 100) return 17;
+  if (distance < 250) return 16;
+  if (distance < 500) return 15;
+  if (distance < 750) return 14.5;
+  if (distance < 1000) return 14;
+  if (distance < 1500) return 13.5;
+  if (distance < 2000) return 13;
+  return 12;
+}
+
+// Splash screen logic
+function startGame() {
+  document.getElementById('splash').classList.add('hidden');
+  document.getElementById('game').classList.remove('hidden');
   shuffleImages();
   nextImage();
-  
+  setTimeout(() => map.invalidateSize(), 0);
+}
+
+const playBtn = document.getElementById('playBtn');
+if (playBtn) {
+  playBtn.addEventListener('click', startGame);
+}
